d4eceb17379a732bef559f78e8ed314cdf4d7447
Himanshu Kumar <himanshu0sharma121@gmail>
2025-12-12 20:38:47 +0530

    initial commit

---
+++ LowLatency.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main()
+{
+    std::cout<<"My name is Harsh Kumar";
+    return 0;
+}
---
+++ firstprogram.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // prints hello world in C++
+    cout << "Hello World in C++";
+    return 0;
+}  
+
+// for adding a multiline comment use this give below enclose the lines in /* and */ṇṇ
+/* Name = Himanshu Kumar
+College Delhi Technological University
+Branch : Computer science and engineering */
+
+/*  1) #include <iostream>
+# (Hashtag) ke baad agar kuch bhi likha hai C++ Program mein toh usko ham bolte hai "Directive"
+And these directibves are processed by a preprocessor, preprocessor kuch nahi hota hai ye ek program hota hai jo ki Compiler Run karta hai, idhar jo #include hai ye hamare preprocessor ko bata raha hai ki iske aage jo bhi kuch likha hua hai usko hame apne code mein include karna hai
+
+____  iostream is a header file , we have a lot of header files in C++ and "iostream is one of them" 
+iostream has input/output , output = cout ...
+
+   2) using namespace std;
+If we don't use "using namespace std;" in our code then we will have to write "cout" statement in a different way
+that is "std::cout"
+ 
+   3) int main(){
+   }   
+There is a particular method of defining a function in C++ that is; 
+First of all we write "return type"
+
+Secondly we write "function name" as we can call a function in different times so its given a specific name"
+
+Thirdly we write "()" parentheses to send any parameters or inputs to our function
+
+Fourthly we write "{ }" (Curly brackets) we write the things which we want to execute inside these curly brackets
+
+*/
+
+   
