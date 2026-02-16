92d07b832e1a80e2f9902d9119896120fd0e554b
Himanshu Kumar <himanshu0sharma121@gmail>
2026-02-16 20:55:55 +0530

    Add/update C++ file

---
+++ Exp_Powers.cpp
@@ -0,0 +1,54 @@
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int num,n;
+    int power;
+    cout<<"Enter the number\n: ";
+    cin>>num;
+    n = num;
+    cout<<"Enter the power to which you want to raise that number\n: ";
+    cin>>power;
+    for (i=1;i<power;i++)
+    cout<<(num=num*n)<<endl;
+    cout<<"So "<<n<<" Raised to Power "<<power<<" is "<<num;
+    return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int num,n;
+    int power;
+    cout<<"Enter the number\n: ";
+    cin>>num;
+    n=num;
+    cout<<"Enter the power to which you want to raise this number\n: ";
+    cin>>power;
+    for(i=1;i<power;i++)
+    cout<<(num=num*n)<<endl;
+}
+*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int n,num;
+    int power;
+    cout<<"Enter the number\n: ";
+    cin>>n;
+    num = n;
+    cout<<"Enter the power you want raise to this number\n: ";
+    cin>>power;
+    for(i=1;i<power;i++)
+    {
+        num=num*n;
+    }
+    cout<<num;
+}*/
+
