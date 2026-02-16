54c4cbb618234aa9e52c5a675bdbab9893928fa5
Himanshu Kumar <himanshu0sharma121@gmail>
2026-02-16 20:58:54 +0530

    Add/update C++ file

---
+++ ALPHABETS.cpp
@@ -0,0 +1,34 @@
+
+// Print alphabets from a to z;
+// ASCII Value of a is 97 
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    char l;
+    for ( l=97;l<=122;l++ )
+    cout<<char(l)<<endl;
+    return 0;
+}*/
+
+// Print alphabets from a to z;
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    char l;
+    for ( l='a';l<='z';l++)
+    cout<<l<<endl;
+    return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    char l;
+    for (l='a';l<='z';l++)
+    cout<<l<<" ";
+    return 0;
+}*/
+
