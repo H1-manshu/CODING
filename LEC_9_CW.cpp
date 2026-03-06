2c510f490503c7ad97207bd0bf1613275080c7a5
Himanshu Kumar <himanshu0sharma121@gmail>
2026-03-06 16:37:01 +0530

    Add/update C++ file

--- LEC_9_CW.cpp
+++ LEC_9_CW.cpp
@@ -737,7 +737,7 @@ int main()
   * *
    *    */
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 int main()
 {
@@ -777,7 +777,7 @@ int main()
     }
     return 0;
 }
-
+*/
 
 /*PRINT By taking input, if the user enters 4 then the below pattern must print)
    * 
@@ -788,3 +788,411 @@ int main()
  * * *
   * *
    *    */
+/*#include <iostream> 
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    int L;
+    cout<<"Which symmetricity's diamond you want to print?\n: ";
+    cin>>L;
+    int k = L - 1;
+    int n = 1;
+    int h = 0;
+    int f = L;
+    for(j=1;j<=L;j++)
+    {
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        k = k - 1;
+        n = n + 1;
+    }
+    for(j=1;j<=L;j++)
+    {
+         for(i=h;i>=1;i=i-1)
+        {
+             cout<<" ";
+        }
+        for(i=1;i<=f;i++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        h = h + 1;
+        f = f - 1;
+    }
+    return 0;
+}
+*/
+
+// Try to print it without introducing the new variables h & f
+/* * 
+  * *
+ * * *
+* * * *
+* * * * 
+ * * *
+  * *
+   *  */
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    int k = 3;
+    int n = 1;
+    for(j=1;j<=4;j++)
+    {
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        k = k - 1;
+        n = n + 1;
+    }
+    k = 3;
+    n = 1;
+    for(j=1;j<=4;j++)
+    {
+        for(i=n-1;i>=1;i=i-1)
+        {
+            cout<<" ";
+        }
+        for(i=k+1;i>=1;i=i-1)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        k = k - 1;
+        n = n + 1;
+    }
+    return 0;
+}*/
+// Print it without introducing the new variables h & f
+/*
+*      *
+**    **
+***  ***
+********
+***  ***
+**    **
+*      *
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    int L;
+    cout<<"Enter the butterfly number\n: ";
+    cin>>L;
+    int k = 2*(L-1);
+    int n = 1;
+    int h = (L-1);
+    int f = 3;
+    for(j=1;j<=L;j++)*/
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    int k = 6;
+    int n = 1;
+    for(j=1;j<=4;j++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        k = k - 2;
+        n = n + 1;
+    }
+    n = 3;
+    k = 2;
+    for(j=1;j<=3;j++)
+    {
+        for(i=1;i<=n;i=i+1)
+        {
+            cout<<"*";
+        }
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        n = n - 1;
+        k = k + 2;
+    }
+}
+*/
+// Try to print it without introducing the new variables h & f 
+// Using taking input from the user if the user enters 4 then print this;
+/* * 
+  * *
+ * * *
+* * * *
+* * * * 
+ * * *
+  * *
+   * 
+*/
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    int L;
+    cout<<"Of which symmetricity's diamond you want to print?\n: ";
+    cin>>L;
+    int k = L-1;
+    int n = 1;
+    for(j=1;j<=L;j++)
+    {
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        k = k - 1;
+        n = n + 1;
+    }
+    k = L;
+    n = 0;
+    for(j=1;j<=L;j++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=k;i++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        k = k - 1;
+        n = n + 1;
+    }
+    return 0;
+}
+*/
+
+// Print it without introducing the new variables h & f
+// If the user enters 4 then print this :
+/*
+*      *
+**    **
+***  ***
+********
+***  ***
+**    **
+*      *
+
+*/
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    int L;
+    cout<<"Of which symmetricity's butterfly you want to print?\n: ";
+    cin>>L;
+    int k = 2*(L-1);
+    int n = 1;
+    for(j=1;j<=L;j++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        n = n + 1;
+        k = k - 2;
+    }
+    n = L-1;
+    k = 2;
+    for(j=1;j<=(L-1);j++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        k = k + 2;
+        n = n - 1;
+    }
+    return 0;
+}*/
+
+/*PRINT USING MORE CLEANER CODE, THAN THE PREVIOUS ONE:
+********
+***  ***
+**    **
+*      *
+*      *
+**    **
+***  ***
+********  */
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    int n = 4;
+    int k = 0;
+    for(j=1;j<=4;j++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        n = n - 1;
+        k = k + 2;
+    }
+    n = 1;
+    k = 6;
+    for(j=1;j<=4;j++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        k = k - 2;
+        n = n + 1;
+    }
+    return 0;
+}*/
+
+/*PRINT USING MORE CLEANER CODE, THAN THE PREVIOUS ONE:
+(By taking input from the user if the user enters 4 then print this:)
+********
+***  ***
+**    **
+*      *
+*      *
+**    **
+***  ***
+********  */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    int L;
+    cout<<"Of what symmetricity's pattern you want to print?\n: ";
+    cin>>L;
+    int n = L;
+    int k = 0;
+    for(j=1;j<=L;j++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        n = n - 1;
+        k = k + 2;
+    }
+    n = 1;
+    k = 2*(L-1);
+    for(j=1;j<=L;j++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        for(i=1;i<=k;i++)
+        {
+            cout<<" ";
+        }
+        for(i=1;i<=n;i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        k = k - 2;
+        n = n + 1;
+    }
+    return 0;
+}
